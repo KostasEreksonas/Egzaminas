f531b02afbe875acb4e19416b6c032ffff1cc11b
KostasEreksonas <k.ereksonas@gmail.com>
2025-01-20 11:11:34 +0200

    Loading komponentas

--- src/app/components/contacts-view/contacts-view.component.ts
+++ src/app/components/contacts-view/contacts-view.component.ts
@@ -4,38 +4,52 @@ import {Contact} from '../../models/contact';
 import {CommonModule} from '@angular/common';
 import {RouterLink} from '@angular/router';
 import {FormsModule} from '@angular/forms';
+import {LoadingComponent} from '../loading/loading.component';
 
 @Component({
   selector: 'app-contacts-view',
-  imports: [CommonModule, RouterLink, FormsModule],
+  imports: [
+    CommonModule,
+    RouterLink,
+    FormsModule,
+    LoadingComponent
+  ],
   templateUrl: './contacts-view.component.html',
   styleUrl: './contacts-view.component.css'
 })
 export class ContactsViewComponent {
   public contacts:Contact[] = [];
   public idToken:string = "";
+  public isLoading = false;
   public filterValue:string = "";
   public filter:string = "";
 
   private loadData(){
+    this.isLoading = true;
     this.contactService.loadContacts().subscribe({
       next:(data)=>{
         this.contacts = data.sort((a,b) => a.surname > b.surname ? 1 : -1);
+        this.isLoading = false;
       }
     })
   }
 
   public filterData() {
+    this.isLoading = true;
     this.contactService.loadContacts().subscribe({
       next:(data)=>{
         if (this.filter === "firstName") {
           this.contacts = data.filter(x => x.firstName === this.filterValue);
+          this.isLoading = false;
         } else if (this.filter === "surname") {
           this.contacts = data.filter(x => x.surname === this.filterValue);
+          this.isLoading = false;
         } else if (this.filter === "companyName") {
           this.contacts = data.filter(x => x.companyName === this.filterValue);
+          this.isLoading = false;
         } else if (this.filter === "") {
           this.contacts = data;
+          this.isLoading = false;
         }
         this.filter = "";
         this.filterValue = "";
